fbef2494abf87cabae9e959a2f2b9384f6c592de
Ahmed Abdullah Helmy <ahmedhelmy82463@gmail.com>
2026-09-05 23:16:28 +0300

    Update frontend API URL to Render cloud

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3001/api', // هذا هو الرابط الصحيح الآن
+  baseURL: 'https://backend-6grl.onrender.com/api',
 });
 
 export default API;
