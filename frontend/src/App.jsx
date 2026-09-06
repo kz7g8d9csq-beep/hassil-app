df8d792f17c9f237d22da4f1a3daf3055029fa3f
Ahmed Abdullah Helmy <ahmedhelmy82463@gmail.com>
2026-09-06 19:32:31 +0300

    طابعة

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+﻿import { useState, useEffect, useRef } from 'react';
 import API from './services/api';
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
